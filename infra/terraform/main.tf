23b6774edaae0b82a8bda41f4088168f2de82d34
Ssomkk <solomiia.trush@gmail.com>
2026-05-24 21:10:28 +0300

    fix(monitoring): update Prometheus configuration and add ingress rules for Grafana and Prometheus

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -64,6 +64,22 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow access to Grafana
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow access to Prometheus
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow server to access Internet (downloading Docker etc)
   egress {
     from_port   = 0
